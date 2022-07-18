7733f8a612f6e75baaf223adee64ba427afca718
milou@penguin <milou@penguin>
2022-07-18 17:42:53 +0200

    add features to galleries and set max-width for main

--- assets/js/photoswap/setPhotoSwipeLightbox.js
+++ assets/js/photoswap/setPhotoSwipeLightbox.js
@@ -1,6 +1,6 @@
 export function setPhotoSwipeLightbox(PhotoSwipeLightbox) {
 
-    const arrowSVG = '<svg aria-hidden="true" class="pswp__icn" viewBox="0 0 100 125" width="100" height="125"><path d="M5,50L50,5l3,3L11,50l42,42l-3,3L5,50z M92,95l3-3L53,50L95,8l-3-3L47,50L92,95z"/></svg>';
+    const arrowSVG = '<svg aria-hidden="true" class="pswp__icn" viewBox="0 0 100 125" width="100" height="125"><path d="M92,95l3-3L53,50L95,8l-3-3L47,50L92,95z"/></svg>';
 
     const options = {
         arrowPrevSVG: arrowSVG,
@@ -8,7 +8,7 @@ export function setPhotoSwipeLightbox(PhotoSwipeLightbox) {
 
         bgOpacity: 0.8,
 
-        counter: false,
+        counter: true,
 
         showHideAnimationType: 'zoom',
         showAnimationDuration: 700,
@@ -45,6 +45,33 @@ export function setPhotoSwipeLightbox(PhotoSwipeLightbox) {
     // Gallery is closed
     lightbox.on('destroy', () => {
         showBadge(lastElWithBadge);
+    });
+
+    lightbox.on('uiRegister', function() {
+      lightbox.pswp.ui.registerElement({
+        name: 'custom-caption',
+        order: 9,
+        isButton: false,
+        appendTo: 'root',
+        html: 'Caption text',
+        onInit: (el, pswp) => {
+          lightbox.pswp.on('change', () => {
+            const currSlideElement = lightbox.pswp.currSlide.data.element;
+            let captionHTML = '';
+            if (currSlideElement) {
+              const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content');
+              if (hiddenCaption) {
+                // get caption from element with class hidden-caption-content
+                captionHTML = hiddenCaption.innerHTML;
+              } else {
+                // get caption from alt attribute
+                captionHTML = currSlideElement.querySelector('img').getAttribute('alt');
+              }
+            }
+            el.innerHTML = captionHTML || '';
+          });
+        }
+      });
     });
 
     lightbox.init();
